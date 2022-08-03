6b7f7787237fd6f19f25acb4c7250a151a4bb70e
Will van Noordt <willvanzero@gmail.com>
2022-08-03 17:05:52 +0100

    som ecomments

--- tests/init/main.cc
+++ tests/init/main.cc
@@ -37,5 +37,34 @@ int main(int argc, char** argv)
     // auto flux_func = symd::vector_func(0.25*(rho_l_v+rho_r_v)*(u_l_v+u_r_v), 0.125*(rho_l_v+rho_r_v)*(u_l_v+u_r_v)*(u_l_v+u_r_v)+0.5*(p_l_v + p_r_v));
     // auto jacobian  = symd::ddx<sym_vec>(flux_func);
     // auto var_list = symd::var_list_union<symd::make_var_list_t<x>::type, symd::make_var_list_t<x, y>::type>::type();
+    
+    
+    //"dream" use case (example: solving a nonlinear system)
+    // enum vars   {ul, uc, ur, x};
+    // enum params {delta_x};
+    // 
+    // 
+    // symd::var_t<ul> ul_v;
+    // symd::var_t<uc> uc_v;
+    // symd::var_t<ur> ur_v;
+    // symd::var_t<x>  x_v;
+    // 
+    // 
+    // symd::param_t<delta_x> dx_p;
+    // 
+    // dx_p=2.5e-3;
+    // 
+    // auto nl  = u_c*(ul_v-ur_v)/(2*dx_p);
+    // auto rhs = (ul_v - 2*uc_v + ur_v)/(dx_p*dx_p) + nl*nl;
+    // 
+    // auto rhs_val = rhs(ul_v=1.0, uc_v=2.0, ur_v=3.0); //-> gives back a double or something
+    // auto vec = symd::var_vector(ul_v, uc_v, ur_v);
+    // 
+    // auto rhs_jac = symd::ddx<vec>(rhs);
+    // auto jac_val = rhs_jac(ul_v=1.0, uc_v=2.0, ur_v=3.0); //-> gives back an array
+    // 
+    // auto u0 = 2*x_v;
+    // //etc etc
+    
     return 0;
 }
